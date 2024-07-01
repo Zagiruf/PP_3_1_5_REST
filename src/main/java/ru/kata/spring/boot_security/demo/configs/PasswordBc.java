3a556df176532e49b35b179c453012c81478389d
Zagiruf <Zagiruf@gmail.com>
2024-07-01 09:44:37 +0500

    committtеt

--- src/main/java/ru/kata/spring/boot_security/demo/configs/PasswordBc.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/PasswordBc.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class PasswordBc {
-    @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
