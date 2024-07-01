252ea9102cbdfbb1f0f218fe8b7705a77ac612ea
Zagiruf <Zagiruf@gmail.com>
2024-07-01 09:55:38 +0500

    committtеtt

--- src/main/java/ru/kata/spring/boot_security/demo/configs/PasswordBc.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/PasswordBc.java
@@ -1,6 +1,5 @@
 package ru.kata.spring.boot_security.demo.configs;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
